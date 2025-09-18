4885979a4639dabb62cf66759cdbebd029367e24
Bruno Da Silva <devbrunodasilva@gmail.com>
2025-09-18 18:06:49 -0300

    Update neovim.lua

--- neovim.lua
+++ neovim.lua
@@ -1,37 +1,21 @@
--- ~/.config/nvim/lua/plugins/theme.lua
-
 return {
-	"EdenEast/nightfox.nvim",
-	lazy = false, -- Carrega o tema na inicialização
-	priority = 1000, -- Garante que seja carregado antes de outros plugins de UI
-	opts = {
-		-- Sua configuração customizada entra aqui
-		palettes = {
-			duskfox = {
-				bg1 = "#FFFFFF", -- Fundo principal preto
-				bg0 = "#1d1d2b", -- Fundo alternativo (janelas flutuantes, statusline, etc.)
-				bg3 = "#1d1d2b",
-				sel0 = "#1d1d2b",
-			},
-		},
-		specs = {
-			all = {
-				inactive = "bg0",
-			},
-			duskfox = {
-				inactive = "#090909", -- Fundo para janelas inativas, levemente mais claro que o preto
-			},
-		},
-		groups = {
-			all = {
-				-- Define o estilo para janelas não focadas (Non-current windows)
-				NormalNC = { fg = "fg1", bg = "inactive" },
-			},
-		},
+	{
+		"EdenEast/nightfox.nvim",
+		config = function()
+			require("nightfox").setup({
+				options = {
+					colorblind = {
+						enable = false,
+						simulate_only = false,
+						severity = {
+							protan = 0, -- 1 = full protanopia, so...
+							deutan = 0,
+							tritan = 0,
+						},
+					},
+				},
+			})
+			vim.cmd("colorscheme duskfox") -- set variant
+		end,
 	},
-	config = function(_, opts)
-		-- Configura e aplica o colorscheme
-		require("nightfox").setup(opts)
-		vim.cmd.colorscheme("duskfox")
-	end,
 }
