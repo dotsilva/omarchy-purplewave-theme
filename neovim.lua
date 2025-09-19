9b46003c2e247d3214f911a49e438253cdbf0798
Bruno Da Silva <devbrunodasilva@gmail.com>
2025-09-18 22:14:12 -0300

    Update neovim.lua

--- neovim.lua
+++ neovim.lua
@@ -1,17 +1,13 @@
--- ~/.config/nvim/lua/plugins/colorscheme.lua
 return {
   {
-    "sainnhe/sonokai",
+    "rose-pine/neovim",
+    name = "rose-pine",
     priority = 1000,
-    -- A função 'init' roda ANTES do plugin carregar, ideal para definir opções
-    init = function()
-      vim.g.sonokai_style = "andromeda"
-    end,
   },
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "sonokai",
+      colorscheme = "rose-pine-moon",
     },
   },
 }
