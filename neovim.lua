022530d9020467e3675ea5371778c7800436e7e8
Bruno Da Silva <devbrunodasilva@gmail.com>
2025-09-18 23:47:20 -0300

    Update neovim.lua

--- neovim.lua
+++ neovim.lua
@@ -1,13 +1,29 @@
 return {
+  -- Instala e configura o Catppuccin
   {
     "catppuccin/nvim",
     name = "catppuccin",
-    priority = 1000,
+    lazy = false,       -- Carrega o tema imediatamente
+    priority = 1000,    -- Garante que ele carregue antes de outros plugins
+    opts = {
+      flavour = "mocha", -- Define a variante Mocha
+      integrations = {
+        alpha = true,    -- ESTA LINHA ATIVA A INTEGRAÇÃO com a tela de início
+        -- Outras integrações recomendadas:
+        gitsigns = true,
+        telescope = true,
+        treesitter = true,
+        mason = true,
+        which_key = true,
+      },
+    },
   },
+
+  -- Diz ao LazyVim para usar o Catppuccin como tema padrão
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "catppuccin-mocha",
+      colorscheme = "catppuccin",
     },
   },
 }
