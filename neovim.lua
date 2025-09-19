fe50cb1b05cb013fb2b81e9a54404bb3511b49ea
Bruno Da Silva <devbrunodasilva@gmail.com>
2025-09-18 22:40:22 -0300

    Update neovim.lua

--- neovim.lua
+++ neovim.lua
@@ -1,13 +1,32 @@
 return {
   {
-    "doums/darcula.nvim",
-    name = "darcula",
+    "Mofiqul/dracula.nvim",
     priority = 1000,
+    opts = {
+      overrides = {
+        -- Cores com contraste suave que definimos antes
+        orange = "#e59f65",
+        red = "#d95c5c",
+        yellow = "#e6db8f",
+
+        -- Seus valores personalizados
+        bg = "#121212",
+        fg = "#d7accd",
+        white = "#ff9acb",
+        gray = "#9e9e9e",
+        purple = "#8673d4", -- <<< SEU NOVO ROXO APLICADO AQUI
+        pink = "#ca2edc",
+        cyan = "#7ea7c9",
+        green = "#8adb8a",
+      },
+
+      italic_comments = true,
+    },
   },
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "darcula",
+      colorscheme = "dracula",
     },
   },
 }
