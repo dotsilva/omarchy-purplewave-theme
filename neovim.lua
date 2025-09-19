ff5a2b22afb132b3148f2a5833aa04844c0630ce
Bruno Da Silva <devbrunodasilva@gmail.com>
2025-09-18 21:05:56 -0300

    Update neovim.lua

--- neovim.lua
+++ neovim.lua
@@ -1,13 +1,20 @@
+-- ~/.config/nvim/lua/plugins/shades_of_purple.lua
 return {
   {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    enabled = false,
-    priority = 1000,
+    "Rigellute/shades-of-purple.vim",
+    name = "shades-of-purple",
+    lazy = false,       -- carregar no startup
+    priority = 1000,    -- garantir antes de outros plugins
     config = function()
-      vim.g.catppuccin_flavour = "mocha" -- "mocha" = fundo escuro
-      require("catppuccin").setup({})
-      vim.cmd("colorscheme catppuccin")
+      vim.cmd("colorscheme shades-of-purple")
     end,
+  },
+
+  -- Override mínimo para LazyVim (consistente com LazyVim 0.15)
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "shades-of-purple",
+    },
   },
 }
