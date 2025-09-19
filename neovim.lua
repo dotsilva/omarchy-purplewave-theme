0578bf686e54dfcb33b0063402fcb569a66f662d
Bruno Da Silva <devbrunodasilva@gmail.com>
2025-09-18 22:46:06 -0300

    Update neovim.lua

--- neovim.lua
+++ neovim.lua
@@ -3,23 +3,13 @@ return {
     "Mofiqul/dracula.nvim",
     priority = 1000,
     opts = {
+      -- Sobrescrevendo apenas o fundo (bg) e o texto principal (fg)
       overrides = {
-        -- Cores com contraste suave que definimos antes
-        orange = "#e59f65",
-        red = "#d95c5c",
-        yellow = "#e6db8f",
-
-        -- Seus valores personalizados
         bg = "#121212",
         fg = "#d7accd",
-        white = "#ff9acb",
-        -- A cor 'gray'/'comment' foi removida e usará o padrão do tema.
-        purple = "#8673d4",
-        pink = "#ca2edc",
-        cyan = "#7ea7c9",
-        green = "#8adb8a",
       },
 
+      -- Mantendo a opção de comentários em itálico
       italic_comments = true,
     },
   },
