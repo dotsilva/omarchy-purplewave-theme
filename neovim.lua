0111a310da6f326944a8a9aa7e3130e7b51af585
Bruno Da Silva <devbrunodasilva@gmail.com>
2025-09-18 23:52:36 -0300

    Update neovim.lua

--- neovim.lua
+++ neovim.lua
@@ -1,28 +1,13 @@
 return {
-  -- Configuração do Tema Catppuccin
   {
     "catppuccin/nvim",
-    lazy = false,
     name = "catppuccin",
     priority = 1000,
-    opts = {
-      flavour = "mocha",
-    },
   },
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "catppuccin",
+      colorscheme = "catppuccin-mocha",
     },
-  },
-
-  -- Configuração para customizar o dashboard
-  {
-    "goolord/alpha-nvim",
-    opts = function(_, opts)
-      -- Altera a cor do cabeçalho
-      opts.config.header.opts.hl = "Function"
-      return opts
-    end,
   },
 }
