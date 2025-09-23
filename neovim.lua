9f1888fb133693bf4a14d9287daf71cb3a497c8d
dotsilva <devbrunodasilva@gmail.com>
2025-09-23 13:40:28 -0300

    commit

--- neovim.lua
+++ neovim.lua
@@ -1,7 +1,9 @@
-{
-  "bjarneo/pixel.nvim",
-  priority = 1000,
-  config = function()
-    vim.cmd.colorscheme("pixel")
-  end,
+return {
+	{
+		"bjarneo/pixel.nvim",
+		priority = 1000,
+		config = function()
+			vim.cmd.colorscheme("pixel")
+		end,
+	},
 }
