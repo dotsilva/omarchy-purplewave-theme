3db0e612a2925f0efcce7b9138acb9f59a319c55
dotsilva <devbrunodasilva@gmail.com>
2025-10-05 18:07:03 -0300

    default neovim theme is now duskfox

--- neovim.lua
+++ neovim.lua
@@ -1,12 +1,12 @@
 return {
-	-- Instala o plugin do tema Dracula
-	{ "Mofiqul/dracula.nvim", name = "dracula", lazy = false, priority = 1000 },
+	"EdenEast/nightfox.nvim",
+	lazy = false, -- Carrega o tema no início para evitar piscar (flash of unstyled content)
+	priority = 1000, -- Garante que seja carregado antes de outros plugins
+	config = function()
+		-- Define a variante que você quer usar
+		vim.g.nightfox_style = "duskfox"
 
-	-- Configura o LazyVim para usar a variante 'dracula-soft'
-	{
-		"LazyVim/LazyVim",
-		opts = {
-			colorscheme = "dracula-soft",
-		},
-	},
+		-- Carrega o colorscheme
+		vim.cmd.colorscheme("duskfox")
+	end,
 }
