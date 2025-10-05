c5e5a2667536d312933f8c1605ce8a0a94f8f581
dotsilva <devbrunodasilva@gmail.com>
2025-10-05 19:03:30 -0300

    possible fix?

--- neovim.lua
+++ neovim.lua
@@ -1,22 +1,20 @@
 return {
-  -- Bloco 1: Garante que as opções globais do Neovim estão corretas
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      options = {
-        termguicolors = true,
-      },
-    },
-  },
+	-- Bloco 1: Desabilita completamente o tema padrão do LazyVim.
+	-- A chave para resolver o problema está aqui.
+	{ "folke/tokyonight.nvim", enabled = false },
 
-  -- Bloco 2: Configura e carrega o plugin do tema
-  {
-    "EdenEast/nightfox.nvim",
-    lazy = false,
-    priority = 1000,
-    config = function()
-      vim.g.nightfox_style = "duskfox"
-      vim.cmd.colorscheme("duskfox")
-    end,
-  },
+	-- Bloco 2: Configura e carrega o seu tema, que agora não terá mais competição.
+	{
+		"EdenEast/nightfox.nvim",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			-- Assegura que o modo de cor está correto (boa prática)
+			vim.opt.termguicolors = true
+
+			-- Carrega o tema duskfox da forma que você confirmou que funciona
+			vim.g.nightfox_style = "duskfox"
+			vim.cmd.colorscheme("duskfox")
+		end,
+	},
 }
