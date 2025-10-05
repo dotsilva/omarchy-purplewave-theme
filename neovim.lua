87f9e357e3afee9c6a1074cb918cc4637288963f
dotsilva <devbrunodasilva@gmail.com>
2025-10-05 18:33:18 -0300

    hm

--- neovim.lua
+++ neovim.lua
@@ -1,10 +1,13 @@
 return {
 	"EdenEast/nightfox.nvim",
-	lazy = false,
-	priority = 1000,
+	lazy = false, -- Carrega o tema no início para evitar piscar (flash of unstyled content)
+	priority = 1000, -- Garante que seja carregado antes de outros plugins
 	config = function()
 		vim.opt.termguicolors = true
+		-- Define a variante que você quer usar
 		vim.g.nightfox_style = "duskfox"
+
+		-- Carrega o colorscheme
 		vim.cmd.colorscheme("duskfox")
 	end,
 }
