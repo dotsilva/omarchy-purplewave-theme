699f44effc7d5737acce58a6b80573f81fdb8560
dotsilva <devbrunodasilva@gmail.com>
2025-10-05 19:29:21 -0300

    hm

--- neovim.lua
+++ neovim.lua
@@ -1,18 +1,8 @@
--- ~/.config/nvim/lua/plugins/theme.lua
 return {
-	"EdenEast/nightfox.nvim",
-	lazy = false, -- Garante que o tema seja carregado no início
-	priority = 1000, -- Define uma alta prioridade de carregamento
-	name = "nightfox",
-	init = function()
-		-- Define o colorscheme para a variante "duskfox"
-		-- O setup é opcional se você não for alterar as configurações padrão [cite: 640]
-		vim.cmd("colorscheme duskfox")
-	end,
-	opts = {
-		-- Exemplo de opção: habilitar fundo transparente
-		-- options = {
-		--   transparent = true,
-		-- },
+	{
+		"EdenEast/nightfox.nvim",
+		config = function()
+			vim.cmd("colorscheme duskfox") -- set duskfox variant
+		end,
 	},
 }
